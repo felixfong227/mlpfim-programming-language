7e464e21e152ce4ee790c79da1c6298528e7a1ad
Felix Fong <moongod101@hotmail.com>
2016-11-21 19:44:09 +0800

    Update the network object to using the core NodeJS module

--- buildfun/network.js
+++ buildfun/network.js
@@ -1,4 +1,6 @@
-var terminal = require("child_process").exec;
+var terminal = require("child_process").exec,
+    https = require("https"),
+    http = require("http")
 module.exports = {
     network: function (code,keyword,line) {
         var call = code.split(".")[1].split(" ")[0];
@@ -9,17 +11,45 @@ module.exports = {
                 var url = code.split("\"")[1].trim();
                 var callbackVariable = code.split("\"")[2].trim();
 
-                terminal(`curl -s ${url}`,function (error,data) {
+                if(url.includes(("https"))){
+                    // https call
+                    https.get(url,function (res) {
 
-                    require("../interperter").running = false;
+                        res.on("data",function (chunk) {
+                            chunk = chunk.toString();
+                            require("../interperter").running = false;
+                            try{
+                                eval(`${callbackVariable} = '${chunk}'`);
+                            }catch (e){
+                                eval(`${callbackVariable} = "${chunk}"`);
+                            }
+                            require("../interperter").main(line);
+                        });
 
-                    try{
-                        eval(`${callbackVariable} = '${data}'`);
-                    }catch (e){
-                        eval(`${callbackVariable} = "${data}"`);
-                    }
-                    require("../interperter").main(line);
-                });
+                    });
+                }else if(url.includes("http")){
+
+                    // https call
+                    http.get(url,function (res) {
+
+                        res.on("data",function (chunk) {
+                            chunk = chunk.toString();
+                            require("../interperter").running = false;
+                            try{
+                                eval(`${callbackVariable} = '${chunk}'`);
+                            }catch (e){
+                                eval(`${callbackVariable} = "${chunk}"`);
+                            }
+                            require("../interperter").main(line);
+                        });
+
+                    });
+
+                }else{
+                    console.log("Please specify where is a http or a https call");
+                    console.on("On code => " + code);
+                    console.log("On line => " + (line + 1));
+                }
 
                 require("../interperter").running = true;
 
@@ -30,20 +60,50 @@ module.exports = {
                 var url = code.split("\"")[1].trim();
                 var callbackVariable = code.split("\"")[2].trim();
 
-                terminal(`curl -s ${url}`,function (error,data) {
+                if(url.includes(("https"))){
+                    // https call
+                    https.get(url,function (res) {
 
-                    require("../interperter").running = false;
-                    try{
-                        eval(`${callbackVariable} = ${data}`);
-                    }catch (e){
-                        eval(`${callbackVariable} = ${data}`);
-                    }
-                    require("../interperter").main(line);
-                });
+                        res.on("data",function (chunk) {
+                            chunk = chunk.toString();
+                            require("../interperter").running = false;
+                            try{
+                                eval(`${callbackVariable} = ${chunk}`);
+                            }catch (e){
+                                eval(`${callbackVariable} = ${chunk}`);
+                            }
+                            require("../interperter").main(line);
+                        });
+
+                    });
+                }else if(url.includes("http")){
+
+                    // https call
+                    http.get(url,function (res) {
+
+                        res.on("data",function (chunk) {
+                            chunk = chunk.toString();
+                            require("../interperter").running = false;
+                            try{
+                                eval(`${callbackVariable} = '${chunk}'`);
+                            }catch (e){
+                                eval(`${callbackVariable} = "${chunk}"`);
+                            }
+                            require("../interperter").main(line);
+                        });
+
+                    });
+
+                }else{
+                    console.log("Please specify where is a http or a https call");
+                    console.on("On code => " + code);
+                    console.log("On line => " + (line + 1));
+                    process.exit();
+                }
 
                 require("../interperter").running = true;
 
-            }
+            },
 
         }
 
