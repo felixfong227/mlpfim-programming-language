52fc50038bc208a5e3fe3814b5aaa48211d8cf09
Felix Fong <moongod101@hotmail.com>
2016-11-23 17:29:53 +0800

    Handling not find error on plugin

--- buildfun/plugin.js
+++ buildfun/plugin.js
@@ -6,11 +6,37 @@ module.exports = {
         var name = code.split(keyword)[1].split(".")[0].trim();
         var calling = code.split(keyword)[1].split(".")[1].split("(")[0].trim();
         var pluginPath = path.join("../plugin/" + name);
+
         try{
+
+            // with args
             var args = code.split(keyword)[1].split(".")[1].split("(")[1].split(")")[0];
-            eval(`require("${pluginPath}").${calling}(${args})`);
+
+            try{
+                eval(`require("${pluginPath}").${calling}(${args})`);
+            }catch (e){
+                if(e.message.includes("Cannot find module")){
+                    console.log("Cannot find module => " + name);
+                    console.log("On code => " + code);
+                    console.log("On line => " + (line + 1))
+                }
+                process.exit();
+            }
+
+
         }catch (e){
-            eval(`require("${pluginPath}").${calling}()`);
+
+            try{
+                eval(`require("${pluginPath}").${calling}()`);
+            }catch (e){
+                if(e.message.includes("Cannot find module")){
+                    console.log("Cannot find module => " + name);
+                    console.log("On code => " + code);
+                    console.log("On line => " + (line + 1))
+                }
+                process.exit();
+            }
+
         }
     }
 
